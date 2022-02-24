b156a41c44546ea3b435082c342770267fc3cb82
Johann Hercovitz <johann.hercovitz@gmail.com>
2022-02-24 16:42:33 +0300

    work beginning

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,42 @@
+    // import { photographerFactory } from "/factories/photographer.js";
+
     async function getPhotographers() {
         // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
+        const photographers = [{
+                    "name": "Ma data test",
+                    "id": 1,
+                    "city": "Paris",
+                    "country": "France",
+                    "tagline": "Ceci est ma data test",
+                    "price": 400,
+                    "portrait": "account.png"
+                },
+                {
+                    "name": "Autre data test",
+                    "id": 2,
+                    "city": "Londres",
+                    "country": "UK",
+                    "tagline": "Ceci est ma data test 2",
+                    "price": 500,
+                    "portrait": "account.png"
+                },
+            ]
+            // et bien retourner le tableau photographers seulement une fois
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers, ...photographers, ...photographers]
+        })
     }
+
+    /////////// FETCH PLUS TARD  
+    // const photographers = [];
+    // fetch('/data/photographers.json').then(response => {
+    //     return response.json();
+    // }).then(data => {
+    //     // Work with JSON data here
+    //     console.log(data);
+    // }).catch(err => {
+    //     this.dataError = true;
+    // });
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
@@ -40,6 +53,5 @@
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
-    
+
     init();
-    
