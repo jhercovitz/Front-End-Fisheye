d07318fddd9a07cfb1d418b997aadfebb93bf90c
Johann Hercovitz <johann.hercovitz@gmail.com>
2022-04-22 16:57:46 +0300

    problem with launchSortMedias

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,6 +1,7 @@
 import { displayModal, closeModal } from "../utils/contactForm.js";
 import { Photographer, Media } from "/scripts/utils/class.js";
 import { Lightbox } from "../Lightbox/lightbox.js";
+import { dropdownEvents } from "../utils/dropdown.js";
 
 const input = document.querySelector("form");
 const idP1 = document.getElementById("p1");
@@ -58,9 +59,25 @@ async function init() {
     sortMedia(currentMedias);
 
     displayPhotographerWork(currentMedias);
-    sortMediaByDate(currentMedias);
-    sortMediaByTitle(currentMedias);
-    sortMediaByLikes(currentMedias);
+
+    function sortMediaByItem() {
+        document.querySelector(".select-selected").addEventListener("change", (e) => {
+            if (e.target.value === "popularité") {
+                sortMediaByLikes(currentMedias)
+                console.log("pop", currentMedias)
+            }
+            if (e.target.value === "date") {
+                sortMediaByDate(currentMedias)
+            }
+            if (e.target.value === "titre") {
+                sortMediaByTitle(currentMedias)
+            }
+        })
+    }
+    // sortMediaByDate(currentMedias);
+    // sortMediaByTitle(currentMedias);
+    // sortMediaByLikes(currentMedias);
+    sortMediaByItem()
     Lightbox.init();
 
 
@@ -131,6 +148,8 @@ function sortMediaByDate(data) {
     })
 }
 
+
+
 // CONTENU DU TAG
 function displayTotalLike(total) {
     const tagContain =
@@ -147,7 +166,6 @@ function displayPrice(price) {
 
 
 // OUVERTURE ET FERMETURE DE LA MODALE
-// voir s'il faut changer de fichier 
 const button = document.getElementById("button");
 button.addEventListener("click", () => {
     document.querySelector('header').style.opacity = "0.6";
@@ -192,89 +210,6 @@ input.addEventListener("input", function(e) {
 });
 
 
-// OUVERTURE DU DROPDOWN
-var x, i, j, l, ll, selElmnt, a, b, c;
-/* Look for any elements with the class "custom-select": */
-x = document.getElementsByClassName("custom-select");
-l = x.length;
-for (i = 0; i < l; i++) {
-    selElmnt = x[i].getElementsByTagName("select")[0];
-    ll = selElmnt.length;
-    /* For each element, create a new DIV that will act as the selected item: */
-    a = document.createElement("DIV");
-    a.setAttribute("class", "select-selected");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    x[i].appendChild(a);
-    /* For each element, create a new DIV that will contain the option list: */
-    b = document.createElement("DIV");
-    b.setAttribute("class", "select-items select-hide");
-    for (j = 1; j < ll; j++) {
-        /* For each option in the original select element,
-        create a new DIV that will act as an option item: */
-        c = document.createElement("DIV");
-        c.innerHTML = selElmnt.options[j].innerHTML;
-        c.addEventListener("click", function(e) {
-            /* When an item is clicked, update the original select box,
-            and the selected item: */
-            var y, i, k, s, h, sl, yl;
-            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-            sl = s.length;
-            h = this.parentNode.previousSibling;
-            for (i = 0; i < sl; i++) {
-                if (s.options[i].innerHTML == this.innerHTML) {
-                    s.selectedIndex = i;
-                    h.innerHTML = this.innerHTML;
-                    y = this.parentNode.getElementsByClassName("same-as-selected");
-                    yl = y.length;
-                    for (k = 0; k < yl; k++) {
-                        y[k].removeAttribute("class");
-                    }
-                    this.setAttribute("class", "same-as-selected");
-                    break;
-                }
-            }
-            h.click();
-        });
-        b.appendChild(c);
-    }
-    x[i].appendChild(b);
-    a.addEventListener("click", function(e) {
-        /* When the select box is clicked, close any other select boxes,
-        and open/close the current select box: */
-        e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-        document.querySelector(".select-selected").style.borderRadius = "5px 5px 0 0";
-    });
-}
 
-function closeAllSelect(elmnt) {
-    /* A function that will close all select boxes in the document,
-    except the current select box: */
-    var x, y, i, xl, yl, arrNo = [];
-    x = document.getElementsByClassName("select-items");
-    y = document.getElementsByClassName("select-selected");
-    xl = x.length;
-    yl = y.length;
-    for (i = 0; i < yl; i++) {
-        if (elmnt == y[i]) {
-            arrNo.push(i)
-        } else {
-            y[i].classList.remove("select-arrow-active");
-        }
-    }
-    for (i = 0; i < xl; i++) {
-        if (arrNo.indexOf(i)) {
-            x[i].classList.add("select-hide");
-            document.querySelector(".select-selected").style.borderRadius = "5px";
-        }
-    }
-}
-
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-document.addEventListener("click", closeAllSelect);
-
-
+dropdownEvents();
 init();
