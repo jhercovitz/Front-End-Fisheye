7fdb9669325f89687e5ce4658fa83c66144118db
Johann Hercovitz <johann.hercovitz@gmail.com>
2022-05-12 14:30:30 +0300

    alt name for media is ok

--- scripts/utils/class.js
+++ scripts/utils/class.js
@@ -68,8 +68,8 @@ export class Media {
         if (this.image) {
             const img = document.createElement('img');
             img.setAttribute("src", this.picture);
-            img.setAttribute("alt", 'image')
             img.setAttribute("data-title", this.title)
+            img.setAttribute("alt", this.title)
             img.classList.add("img_and_video")
             img.setAttribute("tabindex", "0")
             workDiv1.appendChild(img);
@@ -77,7 +77,7 @@ export class Media {
             const videoMedia = document.createElement('video');
             videoMedia.setAttribute("src", this.mp4);
             videoMedia.setAttribute("data-title", this.title)
-            videoMedia.setAttribute("alt", 'video')
+            videoMedia.setAttribute("alt", this.title)
             videoMedia.classList.add("img_and_video")
             videoMedia.setAttribute("controls", "true")
             workDiv1.appendChild(videoMedia);
